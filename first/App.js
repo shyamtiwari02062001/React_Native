1c0009d97043fae30486ccdf1c5c4536b77733e4
shyamtiwari02062001 <shyamtiwari0062001@gmail.com>
2020-05-08 16:36:55 -0400

    Add all my files

---
+++ first/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <ScrollView>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+      <Text style={styles.text}>Shyam Tiwari</Text>
+    </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    flex:1,
+    fontSize:28,
+    color:'white',
+  }
+});
